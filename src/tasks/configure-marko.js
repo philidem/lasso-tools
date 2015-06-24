db0ad94215e8b85efeabe4193ed9935666f334e2
Phillip Gates-Idem <phillip.idem@gmail.com>
2015-06-23 20:00:01 -0400

    Clear taglibs cache when registering new taglibs

--- src/tasks/configure-marko.js
+++ src/tasks/configure-marko.js
@@ -4,6 +4,11 @@ exports.create = function(project) {
         start: function(callback) {
             var markoCompiler = project.util.requireFromProject('marko/compiler');
 
+            // Since we are registering new taglibs, clear any cached lookups
+            // (this is necessary if there were templates that were compiled before
+            // we registered these taglibs which could cause taglibs to be cached)
+            markoCompiler.taglibs.clearCaches();
+            
             markoCompiler.taglibs.registerTaglib(require.resolve('lasso/marko-taglib.json'));
             markoCompiler.taglibs.registerTaglib(require.resolve('browser-refresh-taglib/marko-taglib.json'));
 
