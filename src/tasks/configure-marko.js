7e22c9ad9f8c6b068ca9248b9dfc96bd4a293f03
Phillip Gates-Idem <phillip.idem@gmail.com>
2016-02-21 10:38:58 -0500

    Upgrade to lasso 2.0

--- src/tasks/configure-marko.js
+++ src/tasks/configure-marko.js
@@ -7,10 +7,10 @@ exports.create = function(project) {
             // Since we are registering new taglibs, clear any cached lookups
             // (this is necessary if there were templates that were compiled before
             // we registered these taglibs which could cause taglibs to be cached)
-            markoCompiler.taglibs.clearCaches();
-            
-            markoCompiler.taglibs.registerTaglib(require.resolve('lasso/marko-taglib.json'));
-            markoCompiler.taglibs.registerTaglib(require.resolve('browser-refresh-taglib/marko-taglib.json'));
+            //markoCompiler.taglibs.clearCaches();
+
+            markoCompiler.registerTaglib(require.resolve('lasso/marko.json'));
+            markoCompiler.registerTaglib(require.resolve('browser-refresh-taglib/marko.json'));
 
             process.nextTick(callback);
         }
